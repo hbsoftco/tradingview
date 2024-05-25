90b739f6c4935282f47e748d54e93628fa3b4c39
Hossein <hossein.bajan@gmail.com>
2024-05-25 14:40:00 +0330

    Complete global trading view

--- src/pages/TradingView/index.tsx
+++ src/pages/TradingView/index.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+
 const TradingViewGlobal = () => {
-  return <div>global</div>;
+  const [theme] = useState<"light" | "dark">("dark");
+  const [hideToolbar] = useState(false);
+  const market = "BTCUSD";
+
+  return (
+    <div className="h-screen">
+      <AdvancedRealTimeChart
+        autosize={true}
+        theme={theme}
+        symbol={`${market}`}
+        // symbol={`GATEIO:${market}`}
+        interval="60"
+        timezone="Asia/Tehran"
+        style="1"
+        // locale="fa_IR"
+        enable_publishing={false}
+        withdateranges={true}
+        hide_side_toolbar={hideToolbar}
+        allow_symbol_change={false}
+      />
+    </div>
+  );
 };
 
 export default TradingViewGlobal;
